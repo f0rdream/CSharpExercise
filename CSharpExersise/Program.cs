4a49d6e1a089ffe4785330061b19daa9190e4dd6
fordream001 <littlepigwang@outlook.com>
2016-09-20 16:01:59 +0800

    添加项目文件。

---
+++ CSharpExersise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CITY = "布宜诺斯艾利斯";//常量，城市
+            const string NAME = "列奥波尔多·福图纳托·加尔铁里·卡斯特利";//常量，姓名
+            Console.WriteLine(CITY+"出生在"+NAME+"的一个工人家庭");//使用常量
+        }
+    }
+}
